5783c0514201dde0508612bea0584e4ba5a32716
Ernest Correale <ecorreale@me.com>
2020-02-26 18:26:01 -0500

    final release

--- js/myScript.js
+++ js/myScript.js
@@ -1,17 +1,27 @@
-$(document).ready(StartScript);
+$(document).ready(StartScript); //Wait for DOM to load
 
+
+//Define statrting and ending hours in Miliitaty time
 var _startTime = 9;
 var _endTime = 22;
+
+// Get Current hour to highlight on calendar
 var _currMilHour = moment().format("HH");
+
+//Calendar assiciative Array for past, presednt, future css classes
 var _PPF = { "-1": "past", "NaN": "present", "1": "future" };
 
 
+// Called after DOM loads to begin script
 function StartScript() {
 
     // Set Current date-time in header
     var dToday = moment().format("dddd, MMMM Do");
 
+    //Display in calendar title
     $("#currentDay").text(dToday);
+
+    //Create calendar
     CreateTable();
 }
 
@@ -24,8 +34,10 @@ function CreateTable() {
     var tDiff = 0;
     var newRow;
 
+    // iteratee all hours creating rows with each iteration
     for (IDX = _startTime; IDX <= _endTime; IDX++) {
 
+        //calculate difference between current hour and time displayed
         tDiff = IDX - _currMilHour;
         PPF = tDiff / Math.abs(tDiff);
 
@@ -49,35 +61,42 @@ function tableRow(milHour, ppfIDX) {
     hourCol.text(tHour);
     row.append(hourCol);
 
-    var centerCol = $("<div>");
-    centerCol.addClass("col-md-10");
-    centerCol.addClass(className + " centerCol");
 
+    // Center Column with text input
+    var centerCol = $("<div>");
+    centerCol.addClass(className + " col-md-10 centerCol");
+
+    var lsKey = milHour;
     var textArea = $("<textarea>");
+    textArea.attr("lsKey", lsKey);
+
+    //Check for existing saved data
+    var rowData = localStorage.getItem(lsKey);
+    if (rowData) {
+        textArea.text(rowData);
+    }
+
     centerCol.append(textArea);
     row.append(centerCol);
 
+    //Third column, save button
     var saveCol = $("<div>");
     saveCol.addClass("col-md saveBtnBox");
 
-    var saveBtn = $("<button>");
-    saveBtn.attr("type", "foo");
+    var btnSave = $("<i>");
+    btnSave.addClass("far fa-save");
+    btnSave.attr("onclick", "SaveData(" + lsKey + ")");
 
-
-    var btnText = $("<i>");
-    btnText.addClass("far fa-save");
-    saveBtn.append(btnText);
-
-    saveCol.append(saveBtn);
-
+    saveCol.append(btnSave);
     row.append(saveCol);
 
-
     return row;
-
 }
 
 
-function saveText() {
+function SaveData(lsKey) {
+
+    var rowData = $("textarea[lsKey='" + lsKey + "'").val();
+    localStorage.setItem(lsKey, rowData);
 
 }
